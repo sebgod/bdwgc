6e9c6de5075614affad8b5ea893b7c32881a1896
Paul Bone <paul@bone.id.au>
2014-06-16 21:05:05 +1000

    Add support for enumerating the reachable objects in the heap
    This can be used to do heap profiling, helping the developer work out which
    objects are live or what kinds of objects account for a lot of their heap
    space.
    
    alloc.c:
    include/gc.h:
    include/private/gc_priv.h:
    misc.c:
    reclaim.c:
        As above.
    
    This was originally developed by Peter Wang on 20/5/2011.

--- misc.c
+++ misc.c
@@ -2201,3 +2201,24 @@ GC_API int GC_CALL GC_get_force_unmap_on_gcollect(void)
 {
     return (int)GC_force_unmap_on_gcollect;
 }
+
+GC_API void GC_CALL GC_mercury_write_size_map(FILE *fp)
+{
+    #if defined(_WIN32)
+        SSIZE_T bytes;
+        SSIZE_T limit;
+    #else
+        ssize_t bytes;
+        ssize_t limit;
+    #endif 
+
+    for (limit = MAXOBJBYTES; limit >= 0; limit--) {
+	if (GC_size_map[limit] != 0) {
+	    break;
+	}
+    }
+
+    for (bytes = 1; bytes <= limit; bytes += BYTES_PER_WORD) {
+	fprintf(fp, " %d", (int)GRANULES_TO_WORDS(GC_size_map[bytes]));
+    }
+}
