aea9155ee9d91695b824d7a1a168839c54436d35
Paul Bone <paul@bone.id.au>
2014-06-16 16:52:01 +1000

    Add basic calculation of the total collection time.
    alloc.c:
        Add the duration of each stop the world collection to a new global
        variable which records the total time spent in the GC.
    
    include/gc.h:
        Expose the variable containing the total amount of time spent in
        collection and another variable that enables and disables this
        calculation.
    
    This code was written by Zoltan Somogyi on 18/3/2008.

--- alloc.c
+++ alloc.c
@@ -60,6 +60,12 @@ word GC_non_gc_bytes = 0;  /* Number of bytes not intended to be collected */
 
 word GC_gc_no = 0;
 
+GC_bool         GC_mercury_calc_gc_time = 0;
+			   /* Accumulate total gc time  in      */
+			   /* GC_total_gc_time if set to true.  */
+unsigned long 	GC_total_gc_time = 0;
+			   /* Measured in milliseconds.         */
+
 #ifndef GC_DISABLE_INCREMENTAL
   GC_INNER int GC_incremental = 0;      /* By default, stop the world.  */
 #endif
@@ -427,8 +433,9 @@ GC_INNER GC_bool GC_try_to_collect_inner(GC_stop_func stop_func)
     }
     GC_notify_full_gc();
 #   ifndef SMALL_CONFIG
-      if (GC_print_stats) {
+      if (GC_print_stats || GC_mercury_calc_gc_time) {
         GET_TIME(start_time);
+	if (GC_print_stats)
         GC_log_printf("Initiating full world-stop collection!\n");
       }
 #   endif
@@ -466,11 +473,18 @@ GC_INNER GC_bool GC_try_to_collect_inner(GC_stop_func stop_func)
     }
     GC_finish_collection();
 #   ifndef SMALL_CONFIG
-      if (GC_print_stats) {
+    if (GC_print_stats || GC_mercury_calc_gc_time) {
+        unsigned long cur_gc_time;
         GET_TIME(current_time);
-        GC_log_printf("Complete collection took %lu msecs\n",
-                      MS_TIME_DIFF(current_time,start_time));
-      }
+        cur_gc_time = MS_TIME_DIFF(current_time,start_time);
+        if (GC_print_stats) {
+            GC_log_printf("Complete collection took %lu msecs\n",
+                 cur_gc_time);
+        }
+        if (GC_mercury_calc_gc_time) {
+            GC_total_gc_time += cur_gc_time;
+        }
+    }
 #   endif
     return(TRUE);
 }
